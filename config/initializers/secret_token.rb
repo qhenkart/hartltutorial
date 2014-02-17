215607ff002bd78a78989ad00582ef2aae206222
Quest Henkart <qhenkart@gmail.com>
2014-02-17 11:35:07 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hartlrails::Application.config.secret_key_base = '4aed26937b95b7c6343f9bd4b7da6e394c5eb82af589a7dd98801e6e6a88be133119ce91dd90d8e64aef8dec761e75f05dcc9897a40010bbcaaabdf8d202196a'
